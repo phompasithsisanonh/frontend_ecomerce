93b41e89d54010255ecfac526c9674a5bdc16b53
jon <phompasit2323@gmail.com>
2025-02-20 17:56:32 +0700

    jjjjj

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const local = 'http://localhost:5000'
+const local = 'https://website-ecommercc-easy-shop.vercel.app/api'
 const production = ''
 const api = axios.create({
     baseURL : `${local}/api`
